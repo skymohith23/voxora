85200e6d0c70eeda60defa663ae974718e29f812
skymohith23 <skymohith23@gmail.com>
2025-11-23 18:51:31 +0530

    Update SignCapture.js

---
+++ client/src/components/SignCapture.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignCapture() {
+  return (
+    <div className="box">
+      <h2>Sign Capture</h2>
+      <p>Webcam feed will appear here.</p>
+    </div>
+  );
+}
+
+export default SignCapture;
