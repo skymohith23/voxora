c5f07c81dd450b2bd48f394decc64114f5f3c710
skymohith23 <skymohith23@gmail.com>
2025-11-25 16:21:10 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="home">
       <h1>Welcome to Voxora</h1>
-      <p>Your AI-powered sign language assistant.</p>
-
-      <div className="home-buttons">
-        <Link to="/learn">
-          <button>Learn Sign Language</button>
-        </Link>
-
-        <Link to="/detect">
-          <button>Sign Detection</button>
-        </Link>
-
-        <Link to="/voice">
-          <button>Voice Output</button>
-        </Link>
-      </div>
+      <p>Select any module from above to get started.</p>
     </div>
   );
 }
