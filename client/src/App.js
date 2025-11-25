2a31c3588de2e593e76860165f61474b6917e057
skymohith23 <skymohith23@gmail.com>
2025-11-25 16:01:13 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,31 @@
-import LearnSignLanguage from "./components/LearnSignLanguage";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Learn from "./pages/Learn";
-import SpeechInput from "./components/SpeechInput";
-import SpeechInput from "./components/SpeechInput";
 
+import LearnSignLanguage from "./components/LearnSignLanguage";
+import TextOutput from "./components/TextOutput";
+import VoiceOutput from "./components/VoiceOutput";
 
 function App() {
   return (
     <Router>
-      <nav className="navbar">
-        <h2 className="logo">Voxora</h2>
-        <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/learn">Learn</Link>
-        </div>
-      </nav>
+      <div style={{ padding: "20px" }}>
+        <h1>Voxora</h1>
 
-      <Routes>
-        <Route path="/speech-input" element={<SpeechInput />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/learn" element={<LearnSignLanguage />} />
+        {/* Simple Navigation */}
+        <nav style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
+          <Link to="/learn-sign">Learn Sign</Link>
+          <Link to="/text-output">Text Output</Link>
+          <Link to="/voice-output">Voice Output</Link>
+        </nav>
 
-      </Routes>
+        {/* Routing Section */}
+        <Routes>
+          <Route path="/learn-sign" element={<LearnSignLanguage />} />
+          <Route path="/text-output" element={<TextOutput />} />
+          <Route path="/voice-output" element={<VoiceOutput />} />
+          <Route path="/" element={<h2>Welcome to Voxora</h2>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
