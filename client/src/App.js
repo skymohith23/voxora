6e30d5351a485f71b8361f89b8240e5146b4b43b
skymohith23 <skymohith23@gmail.com>
2025-11-25 16:18:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import LearnSignLanguage from "./components/LearnSignLanguage";
 import SignCapture from "./components/SignCapture";
 import TextOutput from "./components/TextOutput";
 import VoiceOutput from "./components/VoiceOutput";
+import Home from "./components/Home";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/capture" element={<SignCapture />} />
         <Route path="/text" element={<TextOutput />} />
         <Route path="/voice" element={<VoiceOutput />} />
+  <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
