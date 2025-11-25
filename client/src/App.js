391b4e5a36bf665568c863f1881fe10fb799b6c7
skymohith23 <skymohith23@gmail.com>
2025-11-25 16:22:22 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import LearnSignLanguage from "./components/LearnSignLanguage";
-import SignCapture from "./components/SignCapture";
-import TextOutput from "./components/TextOutput";
 import VoiceOutput from "./components/VoiceOutput";
+import TextOutput from "./components/TextOutput";
+import SignCapture from "./components/SignCapture";
 
 function App() {
   return (
-    <Router>
-      <nav className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/learn">Learn</Link>
-        <Link to="/capture">Capture</Link>
-        <Link to="/text">Text Output</Link>
-        <Link to="/voice">Voice Output</Link>
-      </nav>
-
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/learn" element={<LearnSignLanguage />} />
-        <Route path="/capture" element={<SignCapture />} />
-        <Route path="/text" element={<TextOutput />} />
-        <Route path="/voice" element={<VoiceOutput />} />
+        <Route path="/voice-output" element={<VoiceOutput />} />
+        <Route path="/text-output" element={<TextOutput />} />
+        <Route path="/sign-capture" element={<SignCapture />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
