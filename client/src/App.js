54a0ab99bf1f7e7eed86c9df67eda6bb19830dcc
skymohith23 <skymohith23@gmail.com>
2025-11-25 16:19:02 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
 import LearnSignLanguage from "./components/LearnSignLanguage";
 import SignCapture from "./components/SignCapture";
 import TextOutput from "./components/TextOutput";
 import VoiceOutput from "./components/VoiceOutput";
-import Home from "./components/Home";
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LearnSignLanguage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<LearnSignLanguage />} />
         <Route path="/capture" element={<SignCapture />} />
-        <Route path="/text" element={<TextOutput />} />
+        <Route path="/textout" element={<TextOutput />} />
         <Route path="/voice" element={<VoiceOutput />} />
-  <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
